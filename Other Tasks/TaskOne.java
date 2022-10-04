a3777127d8d24138e1c5828084d8a7e765fa6824
Christiana Popoola <60354263+chrispopoola@users.noreply.github.com>
2022-10-04 19:00:31 +0100

    Update and rename TaskOne to TaskOne.java

--- Other Tasks/TaskOne.java
+++ Other Tasks/TaskOne.java
@@ -4,7 +4,7 @@ class TaskOne {
     // write method to find the most frequent integer in the array
 
     public static void main(String[] args) {
-        int[] array = {1, 3, 1, 3, 2, 1};
+        int[] array = {1, 3, 7, 7, 3, 1, 13, 3};
         System.out.println("The most frequently occuring integer is: " + findMostFrequent(array));
     }
 
